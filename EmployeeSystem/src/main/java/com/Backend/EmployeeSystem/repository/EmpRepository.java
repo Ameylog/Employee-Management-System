bdaf5afdd589e0fa522c7c520939386c14350bea
Amey Shende <58946915+Ameylog@users.noreply.github.com>
2023-09-19 23:40:27 +0530

    Delete EmployeeSystem directory

--- EmployeeSystem/src/main/java/com/Backend/EmployeeSystem/repository/EmpRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Backend.EmployeeSystem.repository;
-
-import com.Backend.EmployeeSystem.entity.Emp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpRepository extends JpaRepository<Emp,Integer> {
-}
